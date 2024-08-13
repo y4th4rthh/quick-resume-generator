b2334581e7762a0abc6298b977bf5f7f5262cf65
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-08-14 00:52:39 +0530

    Update ContactUs.jsx

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -142,7 +142,7 @@ const ContactUs = () => {
                             <li>
                                 <a
 
-                                    className="block py-2 px-3 text-white bg-[#4e31aa] rounded md:bg-transparent md:text-[#4e31aa]  md:p-0 "
+                                    className="block py-2 px-3 text-white bg-[#4e31aa] rounded md:bg-transparent hover:text-[#4e31aa] md:text-[#4e31aa]  md:p-0 "
                                     aria-current="page"
                                 >
                                     Contact
