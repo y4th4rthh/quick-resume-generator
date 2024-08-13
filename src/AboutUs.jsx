8c975efe7cb6771919e5b5a5615b2c96e6426e95
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-08-13 20:17:19 +0530

    Update AboutUs.jsx

--- src/AboutUs.jsx
+++ src/AboutUs.jsx
@@ -14,7 +14,7 @@ const AboutUs = () => {
 
     return (
         <div className='font-rubik'>
-            <nav
+           <nav
                 className="bg-white  fixed w-full z-20 top-0 start-0 border-b border-gray-200 "
             >
                 <div
@@ -43,30 +43,7 @@ const AboutUs = () => {
                         >
                             Login
                         </button>
-                        <button
-                            data-collapse-toggle="navbar-sticky"
-                            type="button"
-                            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
-                            aria-controls="navbar-sticky"
-                            aria-expanded="false"
-                        >
-                            <span className="sr-only">Open main menu</span>
-                            <svg
-                                className="w-5 h-5"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 17 14"
-                            >
-                                <path
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="{2}"
-                                    d="M1 1h15M1 7h15M1 13h15"
-                                />
-                            </svg>
-                        </button>
+                        
                     </div>
                     <div
                         className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
@@ -106,10 +83,50 @@ const AboutUs = () => {
                             </li>
                         </ul>
                     </div>
+
+                    <div
+                        className="items-center justify-between md:hidden w-full  md:w-auto md:order-1"
+                        id="navbar-sticky"
+                    >
+                        <ul
+                            className="flex flex-row p-4 md:p-0 mt-4 justify-between font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white "
+                        >
+                            <li>
+                                <a
+                                    href="#"
+                                    onClick={handleNavigateToResumeData}
+                                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#4e31aa] md:p-0  "
+                                >
+                                    Home
+                                </a>
+                            </li>
+
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block py-2 px-3 text-white bg-[#4e31aa] rounded md:bg-transparent  md:text-[#4e31aa] md:p-0 "
+                                    aria-current="page"
+                                >
+                                    About Us
+                                </a>
+                            </li>
+
+                            <li>
+                                <a
+                                    href="#"
+                                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#4e31aa] md:p-0"
+                                    onClick={() => navigate('/contactus')}
+                                >
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
+
                 </div>
             </nav>
-
-            <div className="bg-white mt-11">
+            <div className="bg-white sm:mt-11 mt-28">
                 <section className="bg-[#4e31aa] text-white text-center py-20 px-4">
                     <h1 className="text-4xl font-bold">About Us</h1>
                     <p className="mt-4 text-lg">Learn more about our mission, values, and team.</p>
