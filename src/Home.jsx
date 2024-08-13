494c4cd98eab3efc51ae6c4d0514b59024bbbe1c
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-08-13 20:18:24 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -26,7 +26,11 @@ const Home = () => {
                         onClick={() => navigate('/')}
                         className="flex items-center space-x-3 rtl:space-x-reverse"
                     >
-                      
+                        {/* <img
+                            src="https://img.icons8.com/?size=100&id=VLMGPxgLneP3&format=png&color=000000"
+                            className="h-8"
+                            alt="Flowbite Logo"
+                        /> */}
                         <span
                             className="self-center text-2xl font-semibold whitespace-nowrap text-[#4e31aa] "
                         >
@@ -41,30 +45,7 @@ const Home = () => {
                         >
                             Login
                         </button>
-                        <button
-                            data-collapse-toggle="navbar-sticky"
-                            type="button"
-                            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
-                            aria-controls="navbar-sticky"
-                            aria-expanded="false"
-                        >
-                            <span className="sr-only">Open main menu</span>
-                            <svg
-                                className="w-5 h-5"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 17 14"
-                            >
-                                <path
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="{2}"
-                                    d="M1 1h15M1 7h15M1 13h15"
-                                />
-                            </svg>
-                        </button>
+                        
                     </div>
                     <div
                         className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 "
@@ -104,10 +85,49 @@ const Home = () => {
                             </li>
                         </ul>
                     </div>
+
+                    <div
+                        className="items-center justify-between md:hidden w-full md:w-auto md:order-1 "
+                        id="navbar-sticky"
+                    >
+                        <ul
+                            className="flex flex-row p-4 md:p-0 justify-between mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white "
+                        >
+
+
+                            <li>
+                                <a
+
+                                    className="block py-2 px-3 text-white bg-[#4e31aa]  rounded md:bg-transparent  md:p-0 "
+                                    aria-current="page"
+                                >
+                                    Home
+                                </a>
+                            </li>
+
+                            <li>
+                                <a
+                                    onClick={handleNavigateToAboutUs}
+                                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#4e31aa] md:p-0  "
+                                >
+                                    About Us
+                                </a>
+                            </li>
+
+                            <li>
+                                <a
+                                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#4e31aa] md:p-0 "
+                                    onClick={() => navigate('/contactus')}
+                                >
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
+
                 </div>
             </nav>
-
-            <div className="bg-white mt-8">
+            <div className="bg-white sm:mt-8 mt-28">
                 <section className="bg-[#4e31aa] text-white text-center py-20 px-4">
                     <h1 className="text-4xl font-bold">Create Your Professional Resume in Minutes</h1>
                     <p className="mt-4 text-lg">Get hired faster with our easy-to-use resume builder.</p>
