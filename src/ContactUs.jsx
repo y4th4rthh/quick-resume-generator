0e2b9cae5acaa199fb150a34528da9c48cfaefc4
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-08-13 20:19:20 +0530

    Update ContactUs.jsx

--- src/ContactUs.jsx
+++ src/ContactUs.jsx
@@ -86,7 +86,7 @@ const ContactUs = () => {
 
         <div className='font-rubik'>
             <ToastContainer/>
-            <nav
+          <nav
                 className="bg-white  fixed w-full z-20 top-0 start-0 border-b border-gray-200 "
             >
                 <div
@@ -96,7 +96,11 @@ const ContactUs = () => {
                         onClick={() => navigate('/')}
                         className="flex items-center space-x-3 rtl:space-x-reverse"
                     >
-                        
+                        <img
+                            src="https://img.icons8.com/?size=100&id=TF9VCgblG6vy&format=png&color=000000"
+                            className="h-8"
+                            alt="Flowbite Logo"
+                        />
                         <span
                             className="self-center text-2xl font-semibold whitespace-nowrap text-[#4e31aa] "
                         >
@@ -111,30 +115,7 @@ const ContactUs = () => {
                         >
                             Login
                         </button>
-                        <button
-                            data-collapse-toggle="navbar-sticky"
-                            type="button"
-                            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
-                            aria-controls="navbar-sticky"
-                            aria-expanded="false"
-                        >
-                            <span className="sr-only">Open main menu</span>
-                            <svg
-                                className="w-5 h-5"
-                                aria-hidden="true"
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 17 14"
-                            >
-                                <path
-                                    stroke="currentColor"
-                                    strokeLinecap="round"
-                                    strokeLinejoin="round"
-                                    strokeWidth="{2}"
-                                    d="M1 1h15M1 7h15M1 13h15"
-                                />
-                            </svg>
-                        </button>
+                        
                     </div>
                     <div
                         className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 "
@@ -143,9 +124,6 @@ const ContactUs = () => {
                         <ul
                             className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white "
                         >
-
-
-
 
                             <li>
                                 <a
@@ -176,9 +154,46 @@ const ContactUs = () => {
                             </li>
                         </ul>
                     </div>
+
+                    <div
+                        className="items-center justify-between md:hidden w-full  md:w-auto md:order-1 "
+                        id="navbar-sticky"
+                    >
+                        <ul
+                            className="flex flex-row justify-between p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white "
+                        >
+
+                            <li>
+                                <a
+                                    onClick={() => navigate('/')}
+                                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent  md:hover:text-[#4e31aa] md:p-0  "
+                                >
+                                    Home
+                                </a>
+                            </li>
+
+                            <li>
+                                <a
+                                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#4e31aa] md:p-0 "
+                                    onClick={() => navigate('/aboutus')}
+                                >
+                                    About Us
+                                </a>
+                            </li>
+
+                            <li>
+                                <a
+
+                                    className="block py-2 px-3 text-white bg-[#4e31aa] rounded md:bg-transparent md:text-[#4e31aa]  md:p-0 "
+                                    aria-current="page"
+                                >
+                                    Contact
+                                </a>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
             </nav>
-
             <div className="relative flex items-top justify-center min-h-screen bg-white dark:bg-gray-900 sm:items-center sm:pt-0">
                 <div className="max-w-6xl mx-auto my-auto sm:px-6 lg:px-8">
                     <div className="mt-8 overflow-hidden">
