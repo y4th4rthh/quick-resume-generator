824ae70c6a991d0b9fd82573e5fd23155e5eaed1
PATEL YATHARTH <114477915+y4th4rthh@users.noreply.github.com>
2024-09-27 14:19:11 +0530

    Mobile view ain't responsive enough. Temporary fix now.

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import errorcat from './errorcat.png';
+import errorcat from './errorcat.gif';
 
 
 
