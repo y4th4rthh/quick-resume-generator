bb960343fd2002909c5372b3f6eac169b28ac84a
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-10-21 00:20:36 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -44,7 +44,7 @@ const Home = () => {
                 className="bg-white  fixed w-full z-20 top-0 start-0 border-b border-gray-200 "
             >
                 <div
-                    className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4"
+                    className="w-full flex flex-wrap items-center justify-between mx-auto p-4"
                 >
                     <a
                         onClick={() => navigate('/')}
