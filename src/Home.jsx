43660ac2454a5650c35e930abf6464ee405002a5
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-10-20 23:41:29 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -351,7 +351,30 @@ const Home = () => {
                         >
                             Login
                         </button>
-                        
+                        <button
+                            data-collapse-toggle="navbar-sticky"
+                            type="button"
+                            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 "
+                            aria-controls="navbar-sticky"
+                            aria-expanded="false"
+                        >
+                            <span className="sr-only">Open main menu</span>
+                            <svg
+                                className="w-5 h-5"
+                                aria-hidden="true"
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 17 14"
+                            >
+                                <path
+                                    stroke="currentColor"
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="{2}"
+                                    d="M1 1h15M1 7h15M1 13h15"
+                                />
+                            </svg>
+                        </button>
                     </div>
                     <div
                         className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1 "
@@ -391,50 +414,10 @@ const Home = () => {
                             </li>
                         </ul>
                     </div>
-
-                    <div
-                        className="items-center justify-between md:hidden w-full md:w-auto md:order-1 "
-                        id="navbar-sticky"
-                    >
-                        <ul
-                            className="flex flex-row p-4 md:p-0 justify-between mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white "
-                        >
-
-
-                            <li>
-                                <a
-
-                                    className="block py-2 px-3 text-white bg-[#4e31aa]  rounded md:bg-transparent  md:p-0 "
-                                    aria-current="page"
-                                >
-                                    Home
-                                </a>
-                            </li>
-
-                            <li>
-                                <a
-                                    onClick={handleNavigateToAboutUs}
-                                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#4e31aa] md:p-0  "
-                                >
-                                    About Us
-                                </a>
-                            </li>
-
-                            <li>
-                                <a
-                                    className="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-[#4e31aa] md:p-0 "
-                                    onClick={() => navigate('/contactus')}
-                                >
-                                    Contact
-                                </a>
-                            </li>
-                        </ul>
-                    </div>
-
                 </div>
             </nav>
 
-            <div className="bg-white sm:mt-8 mt-28 ">
+            <div className="bg-white mt-8">
                 <section className="bg-[#4e31aa] text-white text-center py-20 px-4">
                     <h1 className="text-4xl font-bold">Create Your Professional Resume in Minutes</h1>
                     <p className="mt-4 text-lg">Get hired faster with our easy-to-use resume builder.</p>
@@ -451,10 +434,10 @@ const Home = () => {
                     <div className="flex justify-center space-x-8 mt-8 animate-fadeIn">
                         <div className="transition transform hover:scale-110">
                             <h3 className="text-xl font-bold">500+</h3>
-                            <p className="text-gray-700">Color Templates</p>
+                            <p className="text-gray-700">Resume Templates</p>
                         </div>
                         <div className="transition transform hover:scale-110">
-                            <h3 className="text-xl font-bold">1+</h3>
+                            <h3 className="text-xl font-bold">10+</h3>
                             <p className="text-gray-700">Years of Service</p>
                         </div>
                     </div>
