2813c1185a7813a6c29bdfe70045152d228dd7f3
PATEL YATHARTH <114477915+y4th4rthh@users.noreply.github.com>
2024-10-22 15:13:15 +0530

    //

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,9 +13,5 @@ export default {
   },
   variants: {},
   plugins: [],
-  corePlugins: {
-    // Optionally disable built-in scrollbar utilities if you have any custom styling
-    scrollbar: false,
-  },
 }
 
