f3d49320c30d24eda3a93b6865e7569aee17c849
PATEL YATHARTH <114477915+y4th4rthh@users.noreply.github.com>
2024-10-22 15:07:07 +0530

    //

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
   return (
     <>
      <Router >
-       <div className="w-screen" style={{overflowX:'scroll',  msOverflowStyle: 'none', scrollbarWidth: 'none'}}>
+       <div className="w-screen">
             <Routes>
                 <Route exact path="/" element={<Home/>} />
                 <Route path="/login" element={<Login/>} />
