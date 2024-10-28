93d08559544b701109bc3d4efd70f81137b0be7f
Patel Yatharth <114477915+y4th4rthh@users.noreply.github.com>
2024-10-28 11:10:00 +0530

    Update Home.jsx

--- src/Home.jsx
+++ src/Home.jsx
@@ -94,7 +94,7 @@ const Home = () => {
                     </div>
 
                     <div
-                        className="hidden hover:block items-center justify-between md:hidden w-full md:w-auto md:order-1 "
+                        className="hidden group-hover:block items-center justify-between md:hidden w-full md:w-auto md:order-1 "
                         id="navbar-sticky"
                     >
                         <ul
